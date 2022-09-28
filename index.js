7acdd533ccef75732a6ca0bd7298169c8b87b2be
7Nirmal <ra.nirmalkumar96@gmail.com>
2022-09-28 22:27:36 +0530

    razor

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ app.get("/", (request, response)=>{
 })
 
 let instance = new Razorpay({
-    key_id:process.env.key_id,
-    key_secret:process.env.key_secret
+    key_id:process.env.KEY_ID,
+    key_secret:process.env.KEY_SECRET
   });
 app.get("/orders/createOrder/:grandTotal", (request, response)=>{
     let {grandTotal} = request.params
